8e1178db0737f792eef045a13288d52bc756e53c
ted <tstuttle@gmail.com>
2017-07-24 06:21:47 -0700

    Completed statements

--- build.sbt
+++ build.sbt
@@ -5,6 +5,7 @@ version := "1.0"
 scalaVersion := "2.12.1"
 
 libraryDependencies += "com.novocode" % "junit-interface" % "0.11" % Test
+libraryDependencies += "commons-io" % "commons-io" % "2.5"
 
 target := file("c:/tmp/target")
     
--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,11 +1,11 @@
 package com.tstuttle
 
-import java.io.{File, StringWriter, Writer}
+import java.io._
 import java.time.LocalDate
 
 import scala.io.Source
-import scala.util.{Success, Try}
 import Utils.{between, lessEqual}
+import org.apache.commons.io.FileUtils
 
 sealed trait Tx {
     def date: LocalDate
@@ -31,7 +31,16 @@ object Tx {
         typeStr match {
             case "debit" => Debit(d, a, m)
             case "credit" => Credit(d, a, m)
+            case "interest" => Interest(d, a, m)
             case "open" => Open(d, m)
+        }
+    }
+    def typeStr(tx: Tx): String = {
+        tx match {
+            case Credit(_, _, _) => "credit"
+            case Debit(_, _, _) => "debit"
+            case Open(_, _) => "open"
+            case Interest(_, _, _) => "interest"
         }
     }
 }
@@ -46,22 +55,38 @@ case class TxBalance(tx: Tx, balance: Balance) {
     def date: LocalDate = balance.date
 }
 
-case class Statement(start: LocalDate, end: LocalDate, details: Seq[TxBalance])
+case class Statement(acct: Account, start: LocalDate, end: LocalDate, details: Seq[TxBalance])
 
 trait AccountService {
     def updateBalances(through: LocalDate, acct: Account): Account
-    def statement(start: LocalDate, end: LocalDate, acct: Account) : Statement
-    def print(acct: Account, stmt: Statement, writer: Writer): Unit
+    def statement(mode: StatementMode, end: LocalDate, acct: Account) : Statement
+    def print(stmt: Statement, writer: Writer): Unit
 }
 
 trait AccountRepository {
     def query(id: String): Option[Account]
-    def store(a: Account): Try[Account]
+    def store(a: Account): Unit
 }
 
 object AccountServiceImpl extends AccountService {
 
-    override def print(acct: Account, stmt: Statement, writer: Writer): Unit = {
+    override def print(stmt: Statement, w: Writer): Unit = {
+        val totalInterest = stmt.details.tail.map{
+            t => t.tx match {
+                case Interest(_, amt, _) => amt
+                case _ => 0.0d
+            }}.sum
+        def p(s: String): Unit = { w.write(s); w.write("\n") }
+        p(s"Account Owner   : ${stmt.acct.id}")
+        p(s"Statement Period: ${stmt.start} to ${stmt.end}")
+        p(f"Total Interest  : $$$totalInterest%2.2f")
+        p(f"Opening Balance : $$${stmt.details.head.balance.amount}%2.2f")
+        p("Transaction History")
+        p("%10s %10s %10s %35s %10s".format("date", "tx", "amount", "memo", "balance"))
+        def txBalStr(tb: TxBalance) = f"${tb.date}%10s ${Tx.typeStr(tb.tx)}%10s ${tb.tx.amount}%10.2f ${tb.tx.memo}%35s ${tb.balance.amount}%10.2f"
+        for (txBal <- stmt.details.tail) {
+            p(txBalStr(txBal))
+        }
 
     }
 
@@ -70,32 +95,39 @@ object AccountServiceImpl extends AccountService {
         val lastTxBalance = acct.txBalances.reverse.find(b => b.date.equals(Utils.mostRecentMonthEnd(b.date))).
             getOrElse(throw new IllegalStateException(s"Expected at least one month-end balance in ${acct.balanceFile}"))
         println(s"Latest month-end balance: $lastTxBalance")
-        // compute month (start, end) tuples
+        // check if there are any complete months to update
         val start = lastTxBalance.date.plusDays(1)
         val end = Utils.mostRecentMonthEnd(through)
-        Utils.validateRange(start, end)
-        val months = Stream.from(0).map(idx => (start.plusMonths(idx), Utils.monthEnd(start.plusMonths(idx)))).
-            takeWhile{case(_, e) => Utils.lessEqual(e, through)}
-        // build full list of balances
-        // drop head of update, it is dup of last element of bal
-        val newBalances = months.scanLeft(Seq(lastTxBalance)){ case(txBals, mTup) =>
-            computeInterest(mTup._1, mTup._2, txBals.last, acct)
-        }.flatten.tail
+        val newBalances = if (start.isBefore(end)) {
+            // compute month (start, end) tuples
+            val months = Stream.from(0).map(idx => (start.plusMonths(idx), Utils.monthEnd(start.plusMonths(idx)))).
+                takeWhile{case(_, e) => Utils.lessEqual(e, through)}
+            // build full list of balances
+            // drop head of update, it is dup of last element of bal
+            months.scanLeft(Seq(lastTxBalance)){ case(txBals, mTup) =>
+                computeInterest(mTup._1, mTup._2, txBals.last, acct)
+            }.flatten.tail
+        } else Seq.empty
 
         // get immutable history through most recent month-end
         val balanceHistory = acct.txBalances.takeWhile(b => Utils.lessEqual(b.date, lastTxBalance.date))
         // compute current partial period txbal
-        val currentBalances = currentTxs(acct, newBalances.last)
-
+        val currentBalances = currentTxs(acct, newBalances.lastOption.getOrElse(lastTxBalance))
+        // build full list of balances
         val allBalances = balanceHistory ++ newBalances ++ currentBalances
-
-        //allBalances.foreach(println)
-
         acct.copy(txBalances = allBalances)
     }
 
-    override def statement(start: LocalDate, end: LocalDate, acct: Account): Statement = {
-        Statement(start, end, Seq.empty)
+    override def statement(mode: StatementMode, end: LocalDate, acct: Account): Statement = {
+        val start = mode match {
+            case StatementMode.Full => acct.txBalances.head.date
+            case StatementMode.Latest => Utils.monthEnd(Utils.mostRecentMonthEnd(end).minusMonths(1))
+            case _ => throw new IllegalArgumentException(s"Unexpected stmt mode $mode")
+        }
+        Utils.validateRange(start, end)
+        println(s"Generating statement from $start to $end")
+        val txBal = acct.txBalances.filter(txBal => Utils.between(txBal.date, start, end))
+        Statement(acct, start, end, txBal)
     }
 
     private def computeInterest(start: LocalDate,
@@ -123,7 +155,7 @@ object AccountServiceImpl extends AccountService {
         val avgDailyBal = sumBal / numDays
         val intTx = Interest(end,
             avgDailyBal*acct.interestRatePercent/100d*(numDays/365d),
-            s"rate=${acct.interestRatePercent}% avgDailyBal=$avgDailyBal days=$numDays")
+            f"rate=${acct.interestRatePercent}%2.2f adb=$$$avgDailyBal%2.2f days=$numDays")
         val lastBal = byDay.last._2
         txBalances.tail :+ TxBalance(intTx, intTx.adjust(lastBal))
     }
@@ -176,6 +208,28 @@ class FileAccountRepository(file: File) extends AccountRepository {
         }).toSeq
     }
 
+    private def writeBalances(balances: Seq[TxBalance], f: File): Unit = {
+        val bk = new File(f.getParentFile, s"${f.getName}.bak")
+        println(s"Backing up $f to $bk")
+        FileUtils.copyFile(f, bk)
+        println(s"Writing $f")
+        val w = new BufferedWriter(new FileWriter(f))
+        try {
+            w.write("date,txtype,txamount,txmemo,balance")
+            w.newLine()
+            for(txBal <- balances) {
+                // [0] date
+                // [1] txtype
+                // [2] txamount
+                // [3] txmemo
+                // [4] balance
+                w.write(s"${txBal.date.toString},${Tx.typeStr(txBal.tx)}," +
+                    s"${txBal.tx.amount},${txBal.tx.memo},${txBal.balance.amount}")
+                w.newLine()
+            }
+        } finally w.close()
+    }
+
     private def loadAccounts(f: File): Seq[Account] = {
         require(f.exists())
         (for (ln <- Source.fromFile(f).getLines().drop(1)) yield {
@@ -190,10 +244,18 @@ class FileAccountRepository(file: File) extends AccountRepository {
         }).toSeq
     }
 
-    override def store(a: Account): Try[Account] = {
-
-        Success(null)
+    override def store(a: Account): Unit = {
+        writeBalances(a.txBalances, a.balanceFile)
     }
+}
+
+sealed case class StatementMode(value: String)
+object StatementMode {
+    object Full extends StatementMode("Full")
+    object Latest extends StatementMode("Latest")
+    val values = Seq(Full, Latest)
+    def find(s: String): StatementMode = values.find(v => v.value.equalsIgnoreCase(s)).
+        getOrElse(throw new IllegalArgumentException(s"Invalid app mode name $s"))
 }
 
 object App {
@@ -202,23 +264,22 @@ object App {
         // arguments
         val file = new File(args(0))
         val user = args(1)
-        val start = LocalDate.parse(args(2))
-        val end = LocalDate.parse(args(3))
+        val end = LocalDate.now()
+        val mode = StatementMode.find(args(2))
 
         // validate
         require(file.exists(), s"Missing account repo $file")
-        Utils.validateRange(start, end)
 
         // main program logic
         val repo = new FileAccountRepository(file)
         val acct = repo.query(user).getOrElse(throw new IllegalArgumentException(s"Failed to locate account for $user"))
         val newAcct = AccountServiceImpl.updateBalances(end, acct)
-        val stmt = AccountServiceImpl.statement(start,  end, newAcct)
+        val stmt = AccountServiceImpl.statement(mode,  end, newAcct)
 
         // side-effects
         repo.store(newAcct)
         val writer = new StringWriter()
-        AccountServiceImpl.print(newAcct, stmt, writer)
+        AccountServiceImpl.print(stmt, writer)
         println(writer.toString)
     }
 }
